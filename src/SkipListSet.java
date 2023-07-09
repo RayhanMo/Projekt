54fe3b10cea607ed985fcd620e3b668e4c31f047
RayhanM <50557208+RayhanMohammad@users.noreply.github.com>
2023-07-09 17:36:48 -0400

    jmmjll

---
+++ src/SkipListSet.java
@@ -0,0 +1,136 @@
+import java.util.*;
+
+public class SkipListSet <T extends Comparable<T>> implements SortedSet<T> {
+    public int level;
+    public int height;
+    public ArrayList<ArrayList<T>> grid;
+    public SkipListSet()
+    {
+        grid = null;
+    }
+    public SkipListSet(Collection<T> collection)
+    {
+        level = 1;
+        height = 1;
+        grid.get(level - 1) = (ArrayList<T>) collection;
+    }
+    @Override
+    public Comparator<? super T> comparator() {
+        return null;
+    }
+
+    @Override
+    public SortedSet<T> subSet(T fromElement, T toElement) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<T> headSet(T toElement) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<T> tailSet(T fromElement) {
+        return null;
+    }
+
+    @Override
+    public T first() {
+        return null;
+    }
+
+    @Override
+    public T last() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+    private class  SkipListIterator<T extends Comparable<T>> implements Iterator<T>
+    {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public T next() {
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            Iterator.super.remove();
+        }
+    }
+    private class SkipListSetItem<T>
+    {
+        private T data;
+        private T next;
+        private T prev;
+    }
+
+}
